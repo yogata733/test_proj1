6342cc1fa15b144b444d1ea3ac71157e9ea6ca5b
yogata733 <y.ogata.733@gmail.com>
2021-05-08 20:45:51 +0900

    changed project name

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = 'test_project.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'test_project.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
